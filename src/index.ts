dba156d5d8226e8dd153aef37351d755d454e6ea
euriklis <exel_mmm@abv.bg>
2025-11-28 10:04:23 +0200

    Added new features.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export * from "./core";
 export { NVVMCodegen, NVPTXIntrinsics } from "./dialects/nvptx";
 export { AMDGPUCodegen, AMDGPUIntrinsics } from "./dialects/amdgpu";
 export { SPIRVCodegen, SPIRVIntrinsics } from "./dialects/spirv";
+export { ARMCodegen } from "./dialects/arm";
+export { RISCVCodegen } from "./dialects/riscv";
+export { WASMCodegen } from "./dialects/wasm";
